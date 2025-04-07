f6bf7ad3f30759cb78697b242ad99b41e9262665
mayuresh1404 <145109347+mayuresh1404@users.noreply.github.com>
2025-04-07 15:26:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,7 @@ app.get('/', (req, res) => {
   res.send('Hello from DevOps CI/CD Pipeline!');
 });
 
-// Export app for testing
-module.exports = app;
-
-// Start server only if not in test mode
-if (require.main === module) {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
